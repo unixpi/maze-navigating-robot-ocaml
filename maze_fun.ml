75c1ea10e78708aff30f4ab4a118becca5a64d4e
sach <sacha.saint-leger@mail.mcgill.ca>
2015-11-06 17:51:00 -0500

    implemented function 'make_new_move'

--- maze_fun.ml
+++ maze_fun.ml
@@ -10,16 +10,22 @@ let rec maze_robot = ()
 
 let current_pos = ref (0,0)
 let update_current_pos p = (fun c -> match c , !p with
-				     | 'U' , (a,b) -> p := (a+1,b); p
-				     | 'D' , (a,b) -> p := (a-1,b); p
-				     | 'R' , (a,b) -> p := (a,b+1); p
-				     | 'L' , (a,b) -> p := (a,b-1); p
+				     | 'U' , (a,b) -> p := (a,b+1); p
+				     | 'D' , (a,b) -> p := (a,b-1); p
+				     | 'R' , (a,b) -> p := (a+1,b); p
+				     | 'L' , (a,b) -> p := (a-1,b); p
 			   )
 
 let pos_visited = ref [(0,0)];;
 
 let update_pos_visited p = (fun cp -> (p := (cp :: !p)); p);;
-  
+
+let rec visited_already pos_list pos = match pos_list with
+  | [] -> false
+  | h::t -> if (h = pos) then true else (we_visited_already t pos)
+
+let havent_visited_yet pos_list pos = not (we_visited_already pos_list pos)
+					     
 let letters_caught = ref [] 
 
 let remove_first_letter = (fun s -> String.sub s 1 ((String.length s) - 1))
@@ -34,7 +40,25 @@ let rec there_is_a_letter_we_can_move_to  = (fun s -> match (string_to_list_of_c
   | h :: t -> if (h = '#' || h = ' ') then (there_is_a_letter_we_can_move_to (remove_first_letter s))
 	      else
 		true)
+					      
+let rec join l1 l2 = match l1, l2 with
+  | [], [] -> []
+  | h1::t1, h2::t2 -> (h1,h2) :: join t1 t2;;
+  
+let view_to_tuples current_view =
+  let l1 = string_to_list_of_chars (remove_first_letter current_view) in
+  let l2 = ['U';'D';'L';'R'] in
+  join l1 l2;;
 
+(* method below moves to first new square available --> looks Up, then down, then left then right *)
+let rec make_new_move view_tuples current_pos pos_list = match view_tuples, current_pos with
+  | [], _ -> 'K' (* no new move available *)
+  | (_,'#') :: t , _ -> make_new_move t current_pos pos_list
+  | ('U',_) :: t , (x,y) -> if (havent_visited_yet !pos_list (x,y+1)) then 'U' else  make_new_move t current_pos pos_list
+  | ('D',_) :: t , (x,y) -> if (havent_visited_yet !pos_list (x,y-1)) then 'D' else  make_new_move t current_pos pos_list
+  | ('L',_) :: t , (x,y) -> if (havent_visited_yet !pos_list (x-1,y)) then 'L' else  make_new_move t current_pos pos_list
+  | ('R',_) :: t , (x,y) -> if (havent_visited_yet !pos_list (x+1,y)) then 'R' else  make_new_move t current_pos pos_list
+  
 			       
 let check_if_sitting_on_letter = (fun s -> match (String.get s 0) with
 					   | '#' -> (letters_caught := !letters_caught)
@@ -59,7 +83,16 @@ let move_to_first_available_space = (fun s ->
 let make_naive_move = (fun s -> (check_if_sitting_on_letter s); if (there_is_a_letter_we_can_move_to s) then (move_to_first_available_letter s)
 				else
 				  (move_to_first_available_space s))
-    
+let make_slightly_better_move current_pos current_view pos_list =
+  check_if_sitting_on_letter current_view;
+  if (there_is_a_new_letter current_view current_pos pos_list) then (move_to_first_available_letter s)
+  else
+    move_to_first_available_space current_view;;
+(*
+let make_even_better_move current_pos current_view pos_list =
+  check_if_sitting_on_letter current_view;
+  move_to_all_new_locations
+ *)
 
 let sort_char_list_alphabetically = ()	
 let concatenate_list = ()
