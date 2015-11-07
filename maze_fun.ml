6cd45244e517542b5a82ff28a7973a325b60e9fe
sach <sacha.saint-leger@mail.mcgill.ca>
2015-11-07 11:15:41 -0500

    added ability to test robot on slightly tougher maze

--- maze_fun.ml
+++ maze_fun.ml
@@ -187,8 +187,83 @@ let rec lets_play current_view current_pos pos_visited =
          lets_play (update_view move) cp pv)
 
 let start_game () = lets_play starting_view current_pos pos_visited
-
+			      
   (* to do
         1. implement general solution algorithm
         2. create function to sort and return character trophies
    *)
+
+(* testing slightly harder maze  *)
+  
+
+let maze_6_13 = ['#';'#';'#';'#';'#';'#';'#';'#';'#';'#';'#';'#';'#';
+		 '#';' ';' ';' ';'A';' ';' ';' ';'#';' ';' ';' ';'#';
+		 '#';' ';'#';'#';' ';'#';'#';' ';'#';' ';'#';' ';'#';
+		 '#';' ';'Q';'#';' ';'#';' ';' ';' ';'B';' ';' ';'#';
+		 '#';' ';' ';'#';'A';'#';' ';'#';' ';'#';'#';'#';'#';
+		 '#';'#';'#';'#';'#';'#';'#';'#';'#';'#';'#';'#';'#';]
+
+
+let k_6_13 = get_kth_element_from_list maze_6_13
+					  
+let current_6_by_13_view_letters = ref ['A';'#';' ';' ';' '] (* positions [18; 5; 31; 17; 19;] in 6_by_13 maze *)
+
+let current_6_by_13_view_positions = ref [18;5;31;17;19]
+					  									  
+let update_6_by_13_view_positions c move = match move with
+  | 'U' -> c := [((get_kth_element_from_list !current_6_by_13_view_positions 1)-13);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 2)-13);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 3)-13);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 4)-13);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 5)-13)]
+					      
+  | 'D' -> c := [((get_kth_element_from_list !current_6_by_13_view_positions 1)+13);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 2)+13);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 3)+13);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 4)+13);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 5)+13)]
+  
+  | 'L' -> c := [((get_kth_element_from_list !current_6_by_13_view_positions 1)-1);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 2)-1);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 3)-1);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 4)-1);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 5)-1)]
+ 
+  | 'R' -> c := [((get_kth_element_from_list !current_6_by_13_view_positions 1)+1);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 2)+1);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 3)+1);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 4)+1);
+					     ((get_kth_element_from_list !current_6_by_13_view_positions 5)+1)]
+
+let update_6_by_13_view_letters  = (fun c ->
+    c := [k_6_13 (get_kth_element_from_list !current_6_by_13_view_positions 1);
+	  k_6_13 (get_kth_element_from_list !current_6_by_13_view_positions 2);
+	  k_6_13 (get_kth_element_from_list !current_6_by_13_view_positions 3);
+	  k_6_13 (get_kth_element_from_list !current_6_by_13_view_positions 4);
+	  k_6_13 (get_kth_element_from_list !current_6_by_13_view_positions 5);
+	 ])
+
+let starting_view_h = "A#   "
+let current_view_h  = list_of_chars_to_string !current_6_by_13_view_letters;;
+
+  (* to reset *)
+(current_pos := (0,0); pos_visited := [(0,0)];
+current_6_by_13_view_positions := [18;5;31;17;19];
+current_6_by_13_view_letters := ['A';'#';' ';' ';' '];
+letters_caught := []);;
+
+let update_view1 = (fun m -> (update_6_by_13_view_positions current_6_by_13_view_positions m);
+			     update_6_by_13_view_letters current_6_by_13_view_letters; list_of_chars_to_string !current_6_by_13_view_letters)
+
+  
+let rec lets_play_harder current_view current_pos pos_visited =
+  check_if_sitting_on_letter current_view;
+  let move = (make_new_move (view_to_tuples current_view) current_pos pos_visited) in
+    if move = 'K' then Printf.printf "%s\n%!" (end_game ()) else
+         (Printf.printf "%s\n%!" current_view;
+         Printf.printf "%c\n%!" move;
+	 let cp = (update_current_pos current_pos move) in
+	 let pv = (update_pos_visited pos_visited !cp) in
+         lets_play_harder (update_view1 move) cp pv)
+
+let start_game1 () = lets_play_harder starting_view_h current_pos pos_visited
